82875d858b54f458d5a18a4f7fbefd4641d162d8
Keiver-Dev <keiverlunaps11@gmail.com>
2025-04-29 13:08:15 -0300

    Update

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import react from '@astrojs/react';
 import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
-  output: 'static',
+  site: 'https://keiver-dev.github.io',
   base: '/Portafolio-v.1.2/',
   integrations: [
     react(),
