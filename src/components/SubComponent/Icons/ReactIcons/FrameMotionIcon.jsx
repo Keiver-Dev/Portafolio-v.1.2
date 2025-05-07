ddfdcbbaf3b9be3514bfc0649f359c0e60c29502
Keiver-Dev <keiverlunaps11@gmail.com>
2025-05-07 17:17:38 -0300

    Bug Fixed.

--- src/components/SubComponent/Icons/ReactIcons/FrameMotionIcon.jsx
+++ src/components/SubComponent/Icons/ReactIcons/FrameMotionIcon.jsx
@@ -9,10 +9,9 @@ function FrameMotionIcon() {
         viewBox="0 0 24 24"
         fill="none"
         stroke="currentColor"
-        stroke-width="2"
-        stroke-linecap="round"
-        stroke-linejoin="round"
-        class="icon icon-tabler icons-tabler-outline icon-tabler-brand-framer-motion"
+        strokwidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
       >
         <path stroke="none" d="M0 0h24v24H0z" fill="none" />
         <path d="M12 12l-8 -8v16l16 -16v16l-4 -4" />
